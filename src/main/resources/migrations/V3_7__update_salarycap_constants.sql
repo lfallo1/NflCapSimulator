f1210680b0ef9620ffacaf8acc35550a77b6222a
Lance Fallon <fallon.lance@gmail.com>
2015-03-14 23:22:25 -0400

    update db

---
+++ src/main/resources/migrations/V3_7__update_salarycap_constants.sql
@@ -0,0 +1,36 @@
+UPDATE salary_cap_constants set adjusted_cap=161205800 WHERE team = 6 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=153791978 WHERE team = 12 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=167924507 WHERE team = 11 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=151311156 WHERE team = 16 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=144348289 WHERE team = 28 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=145687215 WHERE team = 25 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=151568835 WHERE team = 7 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=150909506 WHERE team = 23 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=142411012 WHERE team = 15 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=144111787 WHERE team = 30 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=155587794 WHERE team = 4 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=149294380 WHERE team = 31 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=152365337 WHERE team = 3 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=147954266 WHERE team = 29 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=144606834 WHERE team = 21 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=151861792 WHERE team = 10 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=147696138 WHERE team = 32 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=145220330 WHERE team = 1 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=149504424 WHERE team = 13 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=148810037 WHERE team = 26 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=144016484 WHERE team = 2 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=146030145 WHERE team = 20 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=144569527 WHERE team = 5 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=145464146 WHERE team = 9 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=148016713 WHERE team = 18 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=149066360 WHERE team = 24 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=159310778 WHERE team = 19 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=143496869 WHERE team = 8 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=143756391 WHERE team = 22 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=142850283 WHERE team = 17 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=144530830 WHERE team = 27 and year = 2015;
+UPDATE salary_cap_constants set adjusted_cap=145582600 WHERE team = 14 and year = 2015;
+
+update salary_cap_constants set carryover_incentive_adjustments = adjusted_cap - salary_cap;
+
+
